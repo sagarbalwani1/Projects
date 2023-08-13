5511807d11c9ecd04ff6a443a27d30d51f3550cd
Sagar Balwani <sagarbalwani1@gmail.com>
2023-08-13 10:26:07 +0530

    Add files via upload

---
+++ PDF_Password_Remover/pdf_pass_remover.py
@@ -0,0 +1,16 @@
+import pikepdf
+
+pdf_loc = input("PDF location: ") 	# Example: C:\\Users\\Desktop\\file1.pdf
+pdf_pass = input("PDF password: ")	# Example: pass1
+
+pdf = pikepdf.open(pdf_loc, password=pdf_pass)
+
+print("\nProcessing...\n")
+
+pdf_save = input("Save file as: ")	# Example: new_file1.pdf
+pdf_loc2 = input("Save location: ")	# Example: C:\\Users\\Desktop
+
+pdf.save(pdf_loc2 + '\\' + pdf_save)
+
+print("The password successfully removed from the PDF")
+print("\aLocation: " + pdf_loc + '\\' + pdf_save)
